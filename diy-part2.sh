b3d445d530ad4be3553fb057779298f87b3c0a9f
oopuuu <75833466+oopuuu@users.noreply.github.com>
2023-01-05 10:38:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,23 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# Modify device Name
+sed -i 's/OpenWrt/Xiaomi AX9000/g' package/base-files/files/bin/config_generate
+
+# Modify root Password
+sed -i 's/root::0:0:99999:7:::/root:$1$wFECtu7f$TN5VYXH0jHoWEH0ing1pY.:18888:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/root:$1$wFECtu7f$TN5VYXH0jHoWEH0ing1pY.:18888:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+
+# Modify Web UI Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify connection number
+echo net.netfilter.nf_conntrack_max=65535 >> package/base-files/files/etc/sysctl.conf
+
+# Modify wifi Name & Password
+# sed -i 's/OpenWrt_2.4G/Redmi_1394_5G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/OpenWrt_5G/Redmi_1394/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/1234567890/zp961203/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
